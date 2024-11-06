340a1e8c937b0810a110f606be9e954e2504e098
Lil-trills <osho.d.joseph@gmail.com>
2024-11-06 22:35:29 +0100

    Update react.js

--- plugins/react.js
+++ plugins/react.js
@@ -1,19 +1,22 @@
-const { bot } = require('../lib/')
-bot(
-  {
-    pattern: 'react ?(.*)',
-    desc: 'React to msg',
-    type: 'misc',
-  },
-  async (message, match) => {
-    if (!match || !message.reply_message) return await message.send('_Example : react ❣_')
-    return await message.send(
-      {
-        text: match,
-        key: message.reply_message.key,
-      },
-      {},
-      'react'
-    )
-  }
-)
+const { bot } = require('../lib/');
+
+function randomDelay(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+bot({
+  pattern: 'react ?(.*)',
+  desc: 'React to msg',
+  type: 'misc',
+}, async (message, match) => {
+  if (!match || !message.reply_message) return await message.send('_Example : react 😂')
+
+  // Generate random delay (e.g., 5-30 seconds)
+  const delay = randomDelay(5000, 30000);
+  await new Promise(resolve => setTimeout(resolve, delay));
+
+  return await message.send({
+    text: match,
+    key: message.reply_message.key,
+  }, {}, 'react');
+});
